89d978beb9c6be8dbb71cd57a9a4c3cd95a1fde4
Samuel FORESTIER <dev@samuel.domains>
2018-02-06 21:37:40 +0100

    Mocks `check_output` call on Hostname entry test

--- test/test_archey_Hostname.py
+++ test/test_archey_Hostname.py
@@ -1,12 +1,17 @@
 
 import unittest
+from unittest.mock import patch
 
 from archey.archey import Hostname
 
 
 class TestHostnameEntry(unittest.TestCase):
-    def test(self):
-        self.assertNotIn(Hostname().value, [None, ''])
+    @patch(
+        'archey.archey.check_output',
+        return_value=b'MY-COOL-LAPTOP\n'
+    )
+    def test(self, check_output_mock):
+        self.assertEqual(Hostname().value, 'MY-COOL-LAPTOP')
 
 
 if __name__ == '__main__':
