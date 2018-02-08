619280c71c5fa51b8cc774d177ddecc927a00e00
Samuel FORESTIER <dev@samuel.domains>
2018-02-08 20:03:49 +0100

    Adds a new test case (`Model`)

---
+++ test/test_archey_Model.py
@@ -0,0 +1,77 @@
+
+import unittest
+from unittest.mock import mock_open, patch
+
+from archey.archey import Model
+
+
+class TestHostnameEntry(unittest.TestCase):
+    """
+    For this test we have to go through the three possibilities :
+    * Laptop / Desktop "regular" environments
+    * Raspberry Pi
+    * Virtual environment (as a VPS or a VM)
+    """
+    @patch(
+        'archey.archey.open',
+        mock_open(
+            read_data='MY-LAPTOP-MODEL\n'
+        )
+    )
+    def test_regular(self):
+        self.assertEqual(Model().value, 'MY-LAPTOP-MODEL')
+
+    def test_raspberry(self):
+        self.return_values = [
+            FileNotFoundError(),  # First `open` call will fail
+            'Hardware\t: HARDWARE\nRevision\t: REVISION\n'
+        ]
+
+        with patch('archey.archey.open', mock_open()) as mock:
+            mock.return_value.read.side_effect = \
+                self._special_func_for_mock_open
+            self.assertEqual(
+                Model().value,
+                'Raspberry Pi HARDWARE (Rev. REVISION)'
+            )
+
+    @patch(
+        'archey.archey.check_output',
+        side_effect=[
+            b'xen\nxen-domU',     # `virt-what` output example
+            b'MY-LAPTOP-MODEL\n'  # `dmidecode` output example
+        ]
+    )
+    def test_virtual(self, check_output_mock):
+        self.return_values = [
+            FileNotFoundError(),      # First `open` call will fail
+            'Hardware\t: HARDWARE\n'  # `Revision` entry is not present
+        ]
+
+        with patch('archey.archey.open', mock_open()) as mock:
+            mock.return_value.read.side_effect = \
+                self._special_func_for_mock_open
+            self.assertEqual(
+                Model().value,
+                'MY-LAPTOP-MODEL (xen, xen-domU)'
+            )
+
+    def _special_func_for_mock_open(self):
+        """
+        This method does not belong to the test cases.
+        It's a special method which allows mocking multiple `io.open` calls.
+        You just have to set the values within a list `self.return_values`.
+        And then :
+        `mock.return_value.read.side_effect = self._special_func_for_mock_open`
+        """
+        return_value = self.return_values.pop(0)
+
+        # Either return value or raise any specified exception
+        if issubclass(return_value.__class__, OSError().__class__):
+            raise return_value
+        else:
+            return return_value
+
+
+if __name__ == '__main__':
+    unittest.main()
