1377231189e105f23f9aa25d52c582bffbecd307
Samuel FORESTIER <dev@samuel.domains>
2018-02-10 09:53:32 +0100

    Adds a new test case (`Kernel`)

---
+++ test/test_archey_Kernel.py
@@ -0,0 +1,22 @@
+
+import unittest
+from unittest.mock import patch
+
+from archey.archey import Kernel
+
+
+class TestDistroEntry(unittest.TestCase):
+    """
+    Here, we mock the `check_output` call and check afterwards
+      that the output is correct.
+    """
+    @patch(
+        'archey.archey.check_output',
+        return_value=b'X.Y.Z-R-arch\n'
+    )
+    def test(self, check_output_mock):
+        self.assertEqual(Kernel().value, 'X.Y.Z-R-arch')
+
+
+if __name__ == '__main__':
+    unittest.main()
