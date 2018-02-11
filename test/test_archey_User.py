a4a7d54ea5dc7de255f8fa44f165c890de1458da
Samuel FORESTIER <dev@samuel.domains>
2018-02-11 18:18:27 +0100

    Reworks `User` test by mocking `getenv` call

--- test/test_archey_User.py
+++ test/test_archey_User.py
@@ -1,5 +1,6 @@
 
 import unittest
+from unittest.mock import patch
 
 from archey.archey import User
 
@@ -8,8 +9,12 @@ class TestUserEntry(unittest.TestCase):
     """
     For this entry, we'll just verify that the output is non-null.
     """
-    def test(self):
-        self.assertNotIn(User().value, [None, ''])
+    @patch(
+        'archey.archey.getenv',
+        return_value='USERNAME'
+    )
+    def test(self, getenv_mock):
+        self.assertEqual(User().value, 'USERNAME')
 
 
 if __name__ == '__main__':
