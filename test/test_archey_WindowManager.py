eee351105d4ddf55117022040f5137847f036d6e
Samuel FORESTIER <dev@samuel.domains>
2018-02-11 18:18:45 +0100

    Improves code coverage of `WindowsManager` test case

--- test/test_archey_WindowManager.py
+++ test/test_archey_WindowManager.py
@@ -29,8 +29,22 @@ class TestHostnameEntry(unittest.TestCase):
         'running',
         'here'
     ])
-    def test_no_wmctrl(self, check_output_mock):
+    def test_no_wmctrl_match(self, check_output_mock):
         self.assertEqual(WindowManager().value, 'Awesome')
+
+    @patch(
+        'archey.archey.check_output',
+        side_effect=FileNotFoundError()  # `wmctrl` call will fail
+    )
+    @patch('archey.archey.PROCESSES', [  # Fake running processes list
+        'some',
+        'weird',  # Mismatch !
+        'programs',
+        'running',
+        'here'
+    ])
+    def test_no_wmctrl_mismatch(self, check_output_mock):
+        self.assertEqual(WindowManager().value, 'Not detected')
 
 
 if __name__ == '__main__':
