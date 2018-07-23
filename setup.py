2a40a2a3e9dd1dea70908913d60b042146e694d6
Samuel FORESTIER <dev@samuel.domains>
2018-07-23 20:52:39 +0200

    Adds supported minor versions of Python to `setup.py` for PyPi meta-data

--- setup.py
+++ setup.py
@@ -28,7 +28,11 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: System'
     ]
 )
