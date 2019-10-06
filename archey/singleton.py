5dc6c9bacb14b7eea065828828eb63fd857d0031
Samuel FORESTIER <dev@samuel.domains>
2019-10-06 17:28:24 +0200

    Explicits that `Singleton` is an abstract meta-class

--- archey/singleton.py
+++ archey/singleton.py
@@ -1,11 +1,16 @@
 """Simple singleton meta-class definition"""
 
-class Singleton(type):
+
+from abc import ABCMeta as AbstractBaseMetaClass
+
+
+class Singleton(AbstractBaseMetaClass):
     """
     Taken from : <https://stackoverflow.com/q/6760685/10599709>
     This meta-class allows us to declare `Configuration` as a singleton.
     This way, we are able to import `Configuration` in multiple modules, ...
     ... whereas it is effectively loaded only once.
+    You cannot instantiate this meta-class directly.
     """
     _instances = {}
     def __call__(cls, *args, **kwargs):
