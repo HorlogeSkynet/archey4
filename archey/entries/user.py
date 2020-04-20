b2cb43a853f97391b93ad70abf94a678a514694d
Samuel FORESTIER <dev@samuel.domains>
2020-04-20 13:17:55 +0200

    [USER] Extends entry support for some specific environments, like Docker

--- archey/entries/user.py
+++ archey/entries/user.py
@@ -2,13 +2,23 @@
 
 import os
 
+from subprocess import CalledProcessError, check_output
+
 from archey.configuration import Configuration
 
 
 class User:
     """Retrieves the session name of the current logged in user"""
     def __init__(self):
-        self.value = os.getenv(
-            'USER',
-            Configuration().get('default_strings')['not_detected']
-        )
+        user = os.getenv('USER')
+        if not user:
+            try:
+                user = check_output(
+                    ['id', '-u', '-n'],
+                    universal_newlines=True
+                ).rstrip()
+            except CalledProcessError:
+                # Should not occur, but who knows ?
+                user = Configuration().get('default_strings')['not_detected']
+
+        self.value = user
--- archey/test/test_archey_user.py
+++ archey/test/test_archey_user.py
@@ -1,5 +1,7 @@
 """Test module for Archey's session user name detection module"""
 
+from subprocess import CalledProcessError
+
 import unittest
 from unittest.mock import patch
 
@@ -9,14 +11,51 @@ from archey.entries.user import User
 class TestUserEntry(unittest.TestCase):
     """
     For this entry, we'll check the output by mocking the `getenv` call.
+    If the environment variable happens to be empty, we fall-back to a fake `id` call.
     """
     @patch(
         'archey.entries.user.os.getenv',
         return_value='USERNAME'
     )
-    def test(self, _):
+    @patch(
+        'archey.entries.user.Configuration.get',
+        return_value={'not_detected': 'Not detected'}
+    )
+    def test_getenv(self, _, __):
         """Simple mock, simple test"""
         self.assertEqual(User().value, 'USERNAME')
+
+    @patch(
+        'archey.entries.user.os.getenv',
+        return_value=''
+    )
+    @patch(
+        'archey.entries.user.check_output',
+        return_value='USERNAME\n'
+    )
+    @patch(
+        'archey.entries.user.Configuration.get',
+        return_value={'not_detected': 'Not detected'}
+    )
+    def test_id_call(self, _, __, ___):
+        """Mock `id` return value and check the correct assignment"""
+        self.assertEqual(User().value, 'USERNAME')
+
+    @patch(
+        'archey.entries.user.os.getenv',
+        return_value=''
+    )
+    @patch(
+        'archey.entries.user.check_output',
+        side_effect=CalledProcessError(1, 'id: ’1000’: no such user')
+    )
+    @patch(
+        'archey.entries.user.Configuration.get',
+        return_value={'not_detected': 'Not detected'}
+    )
+    def test_config_fall_back(self, _, __, ___):
+        """`id` fails, but Archey must not !"""
+        self.assertEqual(User().value, 'Not detected')
 
 
 if __name__ == '__main__':
