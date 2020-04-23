9843c9f773d439fa9b587126c3221ab2d8e9173a
Samuel FORESTIER <dev@samuel.domains>
2020-04-23 19:39:59 +0200

    [UTIL] [PROCESSES] Makes internal `processes` attributes "private"

--- archey/processes.py
+++ archey/processes.py
@@ -12,16 +12,23 @@ class Processes(metaclass=Singleton):
     """At startup, instantiate this class to populate a list of running processes"""
     def __init__(self):
         try:
-            self.processes = check_output(
-                ['ps', '-u' + str(os.getuid()) if os.getuid() != 0 else '-ax',
-                 '-o', 'comm', '--no-headers'], universal_newlines=True
+            self._processes = check_output(
+                [
+                    'ps',
+                    (('-u' + str(os.getuid())) if os.getuid() != 0 else '-ax'),
+                    '-o', 'comm',
+                    '--no-headers'
+                ],
+                universal_newlines=True
             ).splitlines()
 
         except FileNotFoundError:
-            print('Please, install first `procps` on your distribution.',
-                  file=sys.stderr)
+            print(
+                "Please, install first `procps` on your distribution.",
+                file=sys.stderr
+            )
             sys.exit(1)
 
     def get(self):
         """Simple getter to retrieve the processes list"""
-        return self.processes
+        return self._processes
