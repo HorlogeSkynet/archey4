7da2f63f6f1033b902bc8eff2b36d7be026d5489
Michael Bromilow <12384431+ingrinder@users.noreply.github.com>
2020-04-30 23:38:36 +0100

    Fix crash if lacking permissions for /proc/uptime
    This crash occured on locked-down systems, e.g. Android.
    
    Also...
    Adds support for macOS and BSD systems with this entry.
    Adds fallback to the `uptime` command for systems that have it
    (e.g. Android!)
    Adds test cases as appropriate.

--- archey/entries/uptime.py
+++ archey/entries/uptime.py
@@ -1,5 +1,12 @@
 """Uptime detection class"""
 
+
+import re
+import time
+import sys
+from datetime import timedelta
+from subprocess import check_output, CalledProcessError
+
 from archey.entry import Entry
 
 
@@ -8,12 +15,14 @@ class Uptime(Entry):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        with open('/proc/uptime') as file:
-            fuptime = int(file.read().split('.')[0])
+        self._uptime = None
+        self._get_uptime()
 
-        day, fuptime = divmod(fuptime, 86400)
-        hour, fuptime = divmod(fuptime, 3600)
-        minute = fuptime // 60
+        self._fuptime = int(self._uptime.total_seconds())
+
+        day, self._fuptime = divmod(self._fuptime, 86400)
+        hour, self._fuptime = divmod(self._fuptime, 3600)
+        minute = self._fuptime // 60
 
         uptime = ''
         if day:
@@ -46,3 +55,112 @@ class Uptime(Entry):
                 uptime = '< 1 minute'
 
         self.value = uptime
+
+    def _get_uptime(self):
+        """Sets `self._uptime` trying a variety of methods"""
+        # Try the /proc/uptime file
+        try:
+            self._proc_file_uptime()
+            return
+        except (PermissionError, FileNotFoundError):
+            # Can't read /proc/uptime, so not Linux or limited permissions.
+            pass
+
+        # Try the python `time` module clocks
+        try:
+            self._clock_uptime()
+            return
+        except RuntimeError:
+            # Probably Python <3.7, or not *NIX
+            pass
+
+        # FUTURE: Windows support could be added here with the `wmi` or `pywin32` module.
+
+        # Fall back to the `uptime` command
+        self._uptime_cmd()
+
+
+    def _proc_file_uptime(self):
+        """Tries to get uptime using the `/proc/uptime` file"""
+        with open('/proc/uptime') as file:
+            self._uptime = timedelta(
+                seconds=int(file.read().split('.')[0])
+            )
+
+    def _clock_uptime(self):
+        """Tries to get uptime using the clocks from the Python `time` module"""
+        # Try: Linux uptime clock, macOS uptime clock, BSD uptime clock.
+        for clock in ['CLOCK_BOOTTIME', 'CLOCK_UPTIME_RAW', 'CLOCK_UPTIME']:
+            try:
+                self._uptime = timedelta(
+                    seconds=time.clock_gettime(getattr(time, clock))
+                )
+                return
+            except AttributeError:
+                pass
+
+        # Probably Python <3.7, or just not one of the above OSes
+        raise RuntimeError
+
+    def _uptime_cmd(self):
+        """Tries to get uptime by parsing the `uptime` command"""
+        try:
+            uptime_output = check_output('uptime')
+        except (CalledProcessError, FileNotFoundError):
+            # No `uptime` command. Since `procps` is a dependency (which provides `uptime`)
+            # we can just exit here.
+            sys.exit('Required dependency `procps` (or `procps-ng`) missing. Please install.')
+
+        # Unfortunately the output is not designed to be machine-readable...
+        uptime_match = re.search(
+            r"""
+            up\s+?             # match the `up` preceding the uptime, anchors the start of the regex
+            (?:                # non-capture group for days
+               (?P<days>       # 'days' named capture group, captures the days digits
+                  \d+?
+               )
+               \s+?            # match whitespace,
+               day[s]?         # 'day' or 'days',
+               [,\s]+?         # then a comma (if present) followed by more whitespace
+            )?                 # match the days non-capture group 0 or 1 times
+            (?:                # non-capture group for hours & minutes
+               (?:             # non-capture group for just hours
+                  (?P<hours>   # 'hours' named capture group, captures the hours digits
+                     \d+?
+                  )
+                  (?:          # non-capture group for hours:minutes colon or 'hrs' text
+                     :         # i.e. hours followed by either a single colon
+                     |         # OR
+                     \s+?hrs   # one or more whitespace chars non-greedily, followed by 'hrs'
+                  )
+               )?              # match the hours non-capture group 0 or 1 times
+               (?:             # non-capture group for minutes
+                  (?P<minutes> # 'minutes' named capture group, captures the minutes digits
+                     \d+?
+                  )
+                  (?:          # non-capture group for 'min' or 'mins' text
+                     \s+?      # match whitespace,
+                     min[s]?   # followed by 'min' or 'mins
+                  )?           # match the 'mins' text non-capture group 0 or 1 times,
+               )?              # the minutes non-capture group 0 or 1 times
+            )?                 # and the entire hours & minutes non-capture group 0 or 1 times
+            [,\s]*?            # followed by a comma and/or whitespace,
+            \d+?               # some digits for the user count,
+            \s+?               # whitespace between the user count and the text 'user',
+            user               # and the text 'user' - this is to anchor the end of the expression.
+            """,
+            uptime_output,
+            re.VERBOSE
+        )
+
+        times_dict = uptime_match.groupdict()
+        timedelta_args = {
+            'days': 0,
+            'minutes': 0,
+            'hours': 0
+        }
+        for time_unit, value in times_dict.items():
+            if value is not None:
+                timedelta_args[time_unit] = int(value)
+
+        self._uptime = timedelta(**timedelta_args)
--- archey/test/test_archey_uptime.py
+++ archey/test/test_archey_uptime.py
@@ -2,6 +2,7 @@
 
 import unittest
 from unittest.mock import mock_open, patch
+from datetime import timedelta
 
 from archey.entries.uptime import Uptime
 
@@ -62,8 +63,74 @@ class TestUptimeEntry(unittest.TestCase):
         create=True
     )
     def test_days_and_minutes(self):
-        """Test when only days ABD minutes should be displayed"""
+        """Test when only days AND minutes should be displayed"""
         self.assertEqual(Uptime().value, '3 days and 3 minutes')
+
+    @patch(
+        'archey.entries.uptime.open',
+        side_effect=PermissionError(),
+        create=True
+    )
+    # We patch in Linux's CLOCK_BOOTTIME to ensure that
+    # the `getattr` call before `clock_gettime` succeeds
+    @patch(
+        'archey.entries.uptime.time.CLOCK_BOOTTIME',
+        create=True
+    )
+    @patch(
+        'archey.entries.uptime.time.clock_gettime',
+        return_value=1000,
+        create=True
+    )
+    def test_clock_fallback(self, _, __, ___):
+        """
+        Test when we can't access /proc/uptime on Linux/macOS/BSD
+        We only test one clock as all clocks rely on the same builtin `time.clock_gettime` method.
+        """
+        self.assertEqual(Uptime().value, '16 minutes')
+
+    @patch(
+        'archey.entries.uptime.check_output'
+    )
+    def test_linux_clock_fallback(self, check_output_mock):
+        """Test `uptime` command parsing"""
+        # Create an uptime instance to perform testing
+        # It doesn't matter that this will run its __init__
+        uptime_inst = Uptime()
+
+        # Keys: `uptime` outputs; values: expected time deltas.
+        # These outputs have been gathered from various *NIX sytems.
+        # pragma pylint: disable=line-too-long
+        test_cases = {
+            '19:52  up 14 mins, 2 users, load averages: 2.95 4.19 4.31': timedelta(minutes=14),
+            '8:03 up 52 days, 20:47, 3 users, load averages: 1.36 1.42 1.40': timedelta(days=52, hours=20, minutes=47),
+            '22:19 up 54 days, 1 min, 4 users, load averages: 2.08 2.06 2.27': timedelta(days=54, minutes=1),
+            '11:53  up  3:02, 2 users, load averages: 0,32 0,34 0,43': timedelta(hours=3, minutes=2),
+            '12:55pm  up 105 days, 21 hrs,  2 users,  load average: 0.26, 0.26, 0.26': timedelta(days=105, hours=21),
+            '1:41pm  up 105 days, 21:46,  2 users,  load average: 0.28, 0.28, 0.27': timedelta(days=105, hours=21, minutes=46),
+            '8:27  up 1 day, 17:06, 1 user, load averages: 1,01 0,87 0,79': timedelta(days=1, hours=17, minutes=6),
+            '06:27:33 up 4 days,  2:36,  2 users,  load average: 0.16, 0.26, 0.27': timedelta(days=4, hours=2, minutes=36),
+            '06:28:26 up 54 min,  127 users,  load average: 6.34, 6.28, 6.27': timedelta(minutes=54),
+            '17:35pm  up 5 days  9:24,  9 users,  load average: 0.30, 0.28, 0.28': timedelta(days=5, hours=9, minutes=24),
+            '17:36:15 up  8:44,  2 users,  load average: 0.09, 0.30, 0.41': timedelta(hours=8, minutes=44),
+            '03:14:20 up 1 min,  2 users,  load average: 2.28, 1.29, 0.50': timedelta(minutes=1),
+            '04:12:29 up 59 min,  5 users,  load average: 0.06, 0.08, 0.48': timedelta(minutes=59),
+            '05:14:09 up  2:01,  5 users,  load average: 0.13, 0.10, 0.45': timedelta(hours=2, minutes=1),
+            '03:13:19 up 1 day, 0 min,  8 users,  load average: 0.01, 0.04, 0.05': timedelta(days=1),
+            '04:13:19 up 1 day,  1:00,  8 users,  load average: 0.02, 0.05, 0.21': timedelta(days=1, hours=1),
+            '12:49:10 up 25 days, 21:30, 28 users,  load average: 0.50, 0.66, 0.52': timedelta(days=25, hours=21, minutes=30)
+        }
+        # pragma pylint: enable=line-too-long
+
+        for uptime_output, expected_delta in test_cases.items():
+            check_output_mock.return_value = uptime_output
+            uptime_inst._uptime_cmd() # pylint: disable=protected-access
+
+            self.assertEqual(
+                uptime_inst._uptime, # pylint: disable=protected-access
+                expected_delta,
+                msg='`uptime` output: {0}'.format(uptime_output)
+            )
 
 
 if __name__ == '__main__':
