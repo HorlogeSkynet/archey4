25bf6d88b79e43a8960047d0f9a130216c5cf9a4
Samuel FORESTIER <dev@samuel.domains>
2020-05-10 08:56:25 +0200

    [GPU] ([TEST]) Minor documentation improvements

--- archey/entries/gpu.py
+++ archey/entries/gpu.py
@@ -6,22 +6,21 @@ from archey.entry import Entry
 
 
 class GPU(Entry):
-    """Relies on `lspci` to retrieve graphics device(s) information"""
+    """Relies on `lspci` to retrieve graphical device(s) information"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # This list will contain GPU devices detected on this system.
         self.gpu_devices = []
 
-        # Let's try `lspci` here.
+        # Let's try to run `lspci` and parse here.
         self._run_lspci()
 
         self.value = ', '.join(self.gpu_devices) or \
             self._configuration.get('default_strings')['not_detected']
 
     def _run_lspci(self):
-        """Based on `lspci` output, retrieve a list of GPU devices"""
-        # Let's ask `lspci` for a list of current PCI devices on this system.
+        """Based on `lspci` output, retrieve a list of video controllers names"""
         try:
             lspci_output = check_output(
                 ['lspci'],
@@ -30,8 +29,10 @@ class GPU(Entry):
         except (FileNotFoundError, CalledProcessError):
             return
 
-        # Prepares a list of video-only controllers (weighted by the below keys).
+        # We'll be looking for specific video controllers (in the below keys order).
         for video_key in ('3D', 'VGA', 'Display'):
             for pci_device in lspci_output:
+                # If a controller type match...
                 if video_key in pci_device:
+                    # ... adds its name to our final list.
                     self.gpu_devices.append(pci_device.partition(': ')[2])
--- archey/test/test_archey_gpu.py
+++ archey/test/test_archey_gpu.py
@@ -10,7 +10,6 @@ from archey.entries.gpu import GPU
 
 class TestGPUEntry(unittest.TestCase):
     """Here, we mock the `check_output` call to `lspci` to test the logic"""
-
     @patch(
         'archey.entries.gpu.check_output',
         return_value="""\
@@ -20,7 +19,7 @@ XX:YY.H VGA compatible controller: GPU-MODEL-NAME
 XX:YY.H Audio device: DDDDDDDDDDDDDDDD
 """)
     def test_match_vga(self, _):
-        """Simple "VGA" device type matching"""
+        """Simple 'VGA' device type matching"""
         self.assertEqual(GPU().value, 'GPU-MODEL-NAME')
 
     @patch(
@@ -28,7 +27,7 @@ XX:YY.H Audio device: DDDDDDDDDDDDDDDD
         return_value="""\
 XX:YY.H IDE interface: IIIIIIIIIIIIIIII
 XX:YY.H SMBus: BBBBBBBBBBBBBBBB
-XX:YY.H Display controller: ANOTHER MATCHING VIDEO CONTROLLER
+XX:YY.H Display controller: ANOTHER-MATCHING-VIDEO-CONTROLLER
 XX:YY.H Audio device: DDDDDDDDDDDDDDDD
 XX:YY.H 3D controller: 3D GPU-MODEL-NAME TAKES ADVANTAGE
 """)
@@ -36,7 +35,7 @@ XX:YY.H 3D controller: 3D GPU-MODEL-NAME TAKES ADVANTAGE
         """Test detection when there are multiple graphical candidates"""
         self.assertEqual(
             GPU().value,
-            '3D GPU-MODEL-NAME TAKES ADVANTAGE, ANOTHER MATCHING VIDEO CONTROLLER'
+            '3D GPU-MODEL-NAME TAKES ADVANTAGE, ANOTHER-MATCHING-VIDEO-CONTROLLER'
         )
 
     @patch(
