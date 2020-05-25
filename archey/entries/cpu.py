aab40469b830214ef36b55451aff3dc09906ca57
Samuel FORESTIER <dev@samuel.domains>
2020-05-25 10:11:43 +0200

    [CPU] Optimizes entry and makes it compatible with BSD systems (see #69)

--- archey/entries/cpu.py
+++ archey/entries/cpu.py
@@ -12,28 +12,37 @@ class CPU(Entry):
     Parse `/proc/cpuinfo` file to retrieve the model name.
     If no information could be retrieved, calls `lscpu`.
     """
+    _MODEL_NAME_REGEXP = re.compile(
+        r'^model name\s*:\s*(.*)$',
+        flags=re.IGNORECASE | re.MULTILINE
+    )
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        model_name_regex = re.compile(
-            r'^model name\s*:\s*(.*)$',
-            flags=re.IGNORECASE | re.MULTILINE
-        )
-
-        with open('/proc/cpuinfo') as file:
-            cpuinfo = re.search(model_name_regex, file.read())
-
-        # This test case has been built for some ARM architectures (see #29).
-        # Sometimes, `model name` info is not present within `/proc/cpuinfo`.
-        # We use the output of `lscpu` program (util-linux-ng) to retrieve it.
-        if not cpuinfo:
-            cpuinfo = re.search(
-                model_name_regex,
-                check_output(
-                    ['lscpu'],
-                    env={'LANG': 'C'}, universal_newlines=True
-                )
-            )
+        cpuinfo_match = self._read_proc_cpuinfo()
+        if not cpuinfo_match:
+            # This test case has been built for some ARM architectures (see #29).
+            # Sometimes, `model name` info is not present within `/proc/cpuinfo`.
+            # We use the output of `lscpu` program (util-linux-ng) to retrieve it.
+            cpuinfo_match = self._run_lscpu()
 
         # Sometimes CPU model name contains extra ugly white-spaces.
-        self.value = re.sub(r'\s+', ' ', cpuinfo.group(1))
+        self.value = re.sub(r'\s+', ' ', cpuinfo_match.group(1))
+
+    def _read_proc_cpuinfo(self):
+        """Read `/proc/cpuinfo` and search for our model name pattern"""
+        try:
+            with open('/proc/cpuinfo') as f_cpu_info:
+                return self._MODEL_NAME_REGEXP.search(f_cpu_info.read())
+        except (PermissionError, FileNotFoundError):
+            return None
+
+    def _run_lscpu(self):
+        """Same operation but from `lscpu` output"""
+        return self._MODEL_NAME_REGEXP.search(
+            check_output(
+                ['lscpu'],
+                env={'LANG': 'C'}, universal_newlines=True
+            )
+        )
--- archey/test/test_archey_cpu.py
+++ archey/test/test_archey_cpu.py
@@ -77,8 +77,34 @@ model name\t: CPU  MODEL\t  NAME
         create=True
     )
     def test_spaces_squeezing(self):
-        """Test name sanitizing, needed on some platformd"""
+        """Test name sanitizing, needed on some platforms"""
         self.assertEqual(CPU().value, 'CPU MODEL NAME')
+
+    @patch(
+        'archey.entries.cpu.open',
+        side_effect=PermissionError(),
+        create=True
+    )
+    @patch(
+        'archey.entries.cpu.check_output',
+        return_value="""\
+Architecture:        x86_64
+CPU op-mode(s):      32-bit, 64-bit
+Byte Order:          Little Endian
+CPU(s):              4
+On-line CPU(s) list: 0-3
+Thread(s) per core:  X
+Core(s) per socket:  Y
+Socket(s):           1
+NUMA node(s):        1
+Vendor ID:           CPU-VENDOR-NAME
+CPU family:          Z
+Model:               \xde\xad\xbe\xef
+Model name:          CPU-MODEL-NAME-WITHOUT-PROC-CPUINFO
+""")
+    def test_proc_cpuinfo_unreadable(self, _, __):
+        """Check Archey does not crash when `/proc/cpuinfo` is not readable"""
+        self.assertEqual(CPU().value, 'CPU-MODEL-NAME-WITHOUT-PROC-CPUINFO')
 
 
 if __name__ == '__main__':
