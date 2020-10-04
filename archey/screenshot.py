HorlogeSkynet/archey4

commit 73c1f68e39baaa3d734c5ee91a61948e29c70cb3

[FEATURE] Implements `-s` option to make Archey take a screenshot (#75)
Co-authored-by: Michael Bromilow <developer@bromilow.uk>

archey/screenshot.py
@@ -1,0 +1,92 @@
+"""Simple module doing its best as taking a screenshot of the current screen"""
+
+import os
+import sys
+import time
+
+from contextlib import ExitStack
+from datetime import datetime
+from functools import partial
+from subprocess import CalledProcessError, DEVNULL, check_call
+
+
+def take_screenshot(output_file=None):
+    """
+    Simple function trying to take a screenshot using various famous back-end programs.
+    When supported by the found and available back-end, try to honor `output_file`.
+    """
+    if not output_file or os.path.isdir(output_file):
+        # When a directory is provided, we've to force `output_file` to represent a **file** path.
+        output_file = os.path.join(
+            (output_file or os.getcwd()),
+            datetime.now().strftime('archey4_screenshot_%Y-%m-%d_%H.%M.%S.png')
+        )
+
+    # Some programs don't accept specific filename as parameters.
+    # In such cases, we may provide them a target directory instead.
+    output_dir = os.path.dirname(output_file)
+
+    # Back-end programs that _may_ (?) be available across different platforms.
+    screenshot_tools = {
+        'Flameshot': ['flameshot', 'full', '-p', output_dir],
+        'ImageMagick': ['import', '-window', 'root', output_file],
+        'scrot': ['scrot', '-z', output_file],
+        'Shutter': ['shutter', '-f', '-o', output_file, '-e'],
+    }
+
+    # Extends the original screenshot tools dictionary according to current platform.
+    if sys.platform in ('win32', 'cygwin'):
+        screenshot_tools['SnippingTool'] = ['SnippingTool.exe', '/clip']
+    elif sys.platform == 'darwin':
+        screenshot_tools['ScreenCapture'] = [
+            'screencapture',
+            '-x',
+            '-t', output_file.rpart('.')[2],
+            output_file
+        ]
+    else:  # *NIX systems (and others)...
+        screenshot_tools['Escrotum'] = ['escrotum', output_file]
+        screenshot_tools['GNOME-Screenshot'] = ['gnome-screenshot', '-f', output_file]
+        screenshot_tools['grim'] = ['grim', output_file]
+        screenshot_tools['KDE-Spectacle'] = ['spectacle', '-b', '-o', output_file]
+        screenshot_tools['Xfce4-Screenshooter'] = ['xfce4-screenshooter', '-f', '-s', output_dir]
+        screenshot_tools['Screencap (Android)'] = [
+            'screencap',  # Binary available on Android.
+            '-p',         # It only accepts PNG as image output format.
+            (output_file.rpart('.')[0] + '.png')
+        ]
+
+    # This part purposefully blocks so we wait a little bit before taking the screenshot.
+    # It prevents taking a screenshot before Archey's output has appeared.
+    for time_remaining in range(3, 0, -1):
+        taking_sc_str = 'Taking screenshot in {:1d}...'.format(time_remaining)
+        print(taking_sc_str, end='', flush=True)
+        time.sleep(1)
+        print('\r' + ' ' * len(taking_sc_str), end='\r', flush=True)
+    time.sleep(0.5)
+
+    with ExitStack() as defer_stack:
+        for screenshot_tool, screenshot_cmd in screenshot_tools.items():
+            try:
+                check_call(screenshot_cmd, stderr=DEVNULL)
+            except FileNotFoundError:
+                continue
+            except CalledProcessError as process_error:
+                defer_stack.callback(partial(
+                    print,
+                    'Couldn\'t take a screenshot with {}: \"{}\".'.format(
+                        screenshot_tool, process_error
+                    ),
+                    file=sys.stderr
+                ))
+                continue
+            break
+        else:
+            defer_stack.callback(partial(
+                print,
+                """\
+Sorry, we couldn\'t find any supported program to take a screenshot on your system.
+Please install one of the following and try again: {}.\
+""".format(', '.join(screenshot_tools.keys())),
+                file=sys.stderr
+            ))

archey.1
@@ -31,7 +31,7 @@ Remain \fImaintained\fR, \fIcommunity-driven\fR and
 .IP "-h, --help"
 show help message and exit
 
-.IP "-c, --config-path"
+.IP "-c, --config-path PATH"
 path to a configuration file, or a directory containing a `config.json`
 
 .IP "-d, --distribution IDENTIFIER"
@@ -40,6 +40,10 @@ supported distribution identifier to show the logo of, pass `unknown` to list th
 .IP "-j, --json"
 output entries data to JSON format, use multiple times to increase
 indentation
+
+.IP "-s, --screenshot [FILENAME]"
+take a screenshot once execution is done, optionally specify a target
+path
 
 .IP "-v, --version"
 show program's version number and exit

archey/__main__.py
@@ -18,6 +18,7 @@ from archey.output import Output
 from archey.configuration import Configuration
 from archey.distributions import Distributions
 from archey.processes import Processes
+from archey.screenshot import take_screenshot
 from archey.entries.user import User as e_User
 from archey.entries.hostname import Hostname as e_Hostname
 from archey.entries.model import Model as e_Model
@@ -71,6 +72,7 @@ def args_parsing():
     parser = argparse.ArgumentParser(prog='archey')
     parser.add_argument(
         '-c', '--config-path',
+        metavar='PATH',
         help='path to a configuration file, or a directory containing a `config.json`'
     )
     parser.add_argument(
@@ -83,6 +85,13 @@ def args_parsing():
         '-j', '--json',
         action='count',
         help='output entries data to JSON format, use multiple times to increase indentation'
+    )
+    parser.add_argument(
+        '-s', '--screenshot',
+        metavar='PATH',
+        nargs='?',
+        const=False,
+        help='take a screenshot once execution is done, optionally specify a target path'
     )
     parser.add_argument(
         '-v', '--version',
@@ -136,6 +145,11 @@ def main():
             output.add_entry(entry_instance)
 
     output.output()
+
+    # Has the screenshot flag been specified ?
+    if args.screenshot is not None:
+        # If so, but still _falsy_, pass `None` as no output file has been specified by the user.
+        take_screenshot((args.screenshot or None))
 
 
 if __name__ == '__main__':

setup.py
@@ -16,7 +16,7 @@ setup(
     name='archey4',
     version=__version__.lstrip('v'),
     description='Archey is a simple system information tool written in Python',
-    keywords='archey python3 linux system-information monitoring',
+    keywords='archey python3 linux system-information monitoring screenshot',
     url='https://github.com/HorlogeSkynet/archey4',
     author='Samuel Forestier',  # Not alone
     author_email='dev+archey@samuel.domains',

commit f1523ff9b086e291f5af9428736868b3b42d27f0

[SCREENSHOT] Fixes usages of unknown `.rpart` method
> Fixes a bug introduced in #75, released in v4.8.0

[skip ci]

archey/screenshot.py
@@ -41,7 +41,7 @@ def take_screenshot(output_file=None):
         screenshot_tools['ScreenCapture'] = [
             'screencapture',
             '-x',
-            '-t', output_file.rpart('.')[2],
+            '-t', output_file.rpartition('.')[2],
             output_file
         ]
     else:  # *NIX systems (and others)...
@@ -53,7 +53,7 @@ def take_screenshot(output_file=None):
         screenshot_tools['Screencap (Android)'] = [
             'screencap',  # Binary available on Android.
             '-p',         # It only accepts PNG as image output format.
-            (output_file.rpart('.')[0] + '.png')
+            (output_file.rpartition('.')[0] + '.png')
         ]
 
     # This part purposefully blocks so we wait a little bit before taking the screenshot.