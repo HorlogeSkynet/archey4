HorlogeSkynet/archey4

commit 9d337e862876a714264b20f267c557f30b9961c3

[CHORE][LOGOS] Sets right-padding in `Output` instead of logos modules

archey/output.py
@@ -154,7 +154,7 @@ class Output:
 
         # Merge entry results to the distribution logo.
         logo_with_entries = os.linesep.join([
-            logo_part + entry_part
+            f"{logo_part}   {entry_part}"
             for logo_part, entry_part in zip(self._logo, self._results)
         ])
 

archey/logos/alpine.py
@@ -6,24 +6,24 @@ from archey.colors import Colors
 COLORS = [Colors.BLUE_BRIGHT]
 
 LOGO = [
-    """{c[0]}        .hddddddddddddddddddddddh.          """,
-    """{c[0]}       :dddddddddddddddddddddddddd:         """,
-    """{c[0]}      /dddddddddddddddddddddddddddd/        """,
-    """{c[0]}     +dddddddddddddddddddddddddddddd+       """,
-    """{c[0]}   `sdddddddddddddddddddddddddddddddds`     """,
-    """{c[0]}  `ydddddddddddd++hdddddddddddddddddddy`    """,
-    """{c[0]} .hddddddddddd+`  `+ddddh:-sdddddddddddh.   """,
-    """{c[0]} hdddddddddd+`      `+y:    .sddddddddddh   """,
-    """{c[0]} ddddddddh+`   `//`   `.`     -sddddddddd   """,
-    """{c[0]} ddddddh+`   `/hddh/`   `:s-    -sddddddd   """,
-    """{c[0]} ddddh+`   `/+/dddddh/`   `+s-    -sddddd   """,
-    """{c[0]} ddd+`   `/o` :dddddddh/`   `oy-    .yddd   """,
-    """{c[0]} hdddyo+ohddyosdddddddddho+oydddy++ohdddh   """,
-    """{c[0]} .hddddddddddddddddddddddddddddddddddddh.   """,
-    """{c[0]}  `yddddddddddddddddddddddddddddddddddy`    """,
-    """{c[0]}   `sdddddddddddddddddddddddddddddddds`     """,
-    """{c[0]}     +dddddddddddddddddddddddddddddd+       """,
-    """{c[0]}      /dddddddddddddddddddddddddddd/        """,
-    """{c[0]}       :dddddddddddddddddddddddddd:         """,
-    """{c[0]}        .hddddddddddddddddddddddh.          """
+     """{c[0]}        .hddddddddddddddddddddddh.       """,
+     """{c[0]}       :dddddddddddddddddddddddddd:      """,
+     """{c[0]}      /dddddddddddddddddddddddddddd/     """,
+     """{c[0]}     +dddddddddddddddddddddddddddddd+    """,
+     """{c[0]}   `sdddddddddddddddddddddddddddddddds`  """,
+     """{c[0]}  `ydddddddddddd++hdddddddddddddddddddy` """,
+     """{c[0]} .hddddddddddd+`  `+ddddh:-sdddddddddddh.""",
+     """{c[0]} hdddddddddd+`      `+y:    .sddddddddddh""",
+     """{c[0]} ddddddddh+`   `//`   `.`     -sddddddddd""",
+     """{c[0]} ddddddh+`   `/hddh/`   `:s-    -sddddddd""",
+     """{c[0]} ddddh+`   `/+/dddddh/`   `+s-    -sddddd""",
+     """{c[0]} ddd+`   `/o` :dddddddh/`   `oy-    .yddd""",
+     """{c[0]} hdddyo+ohddyosdddddddddho+oydddy++ohdddh""",
+     """{c[0]} .hddddddddddddddddddddddddddddddddddddh.""",
+     """{c[0]}  `yddddddddddddddddddddddddddddddddddy` """,
+     """{c[0]}   `sdddddddddddddddddddddddddddddddds`  """,
+     """{c[0]}     +dddddddddddddddddddddddddddddd+    """,
+     """{c[0]}      /dddddddddddddddddddddddddddd/     """,
+     """{c[0]}       :dddddddddddddddddddddddddd:      """,
+     """{c[0]}        .hddddddddddddddddddddddh.       """
 ]

archey/logos/android.py
@@ -6,22 +6,22 @@ from archey.colors import Colors
 COLORS = [Colors.GREEN_BRIGHT, Colors.WHITE_BRIGHT]
 
 LOGO = [
-    """{c[0]}          -o          o-            """,
-    """{c[0]}           +hydNNNNdyh+             """,
-    """{c[0]}         +mMMMMMMMMMMMMm+           """,
-    """{c[0]}       `dMM{c[1]}m:{c[0]}NMMMMMMN{c[1]}:m{c[0]}MMd`         """,
-    """{c[0]}       hMMMMMMMMMMMMMMMMMMh         """,
-    """{c[0]}   ..  yyyyyyyyyyyyyyyyyyyy  ..     """,
-    """{c[0]} .mMMm`MMMMMMMMMMMMMMMMMMMM`mMMm.   """,
-    """{c[0]} :MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:   """,
-    """{c[0]} :MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:   """,
-    """{c[0]} :MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:   """,
-    """{c[0]} :MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:   """,
-    """{c[0]} -MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM-   """,
-    """{c[0]}  +yy+ MMMMMMMMMMMMMMMMMMMM +yy+    """,
-    """{c[0]}       mMMMMMMMMMMMMMMMMMMm         """,
-    """{c[0]}       `/++MMMMh++hMMMM++/`         """,
-    """{c[0]}           MMMMo  oMMMM             """,
-    """{c[0]}           MMMMo  oMMMM             """,
-    """{c[0]}           oNMm-  -mMNs             """
+     """{c[0]}          -o          o-         """,
+     """{c[0]}           +hydNNNNdyh+          """,
+     """{c[0]}         +mMMMMMMMMMMMMm+        """,
+     """{c[0]}       `dMM{c[1]}m:{c[0]}NMMMMMMN{c[1]}:m{c[0]}MMd`      """,
+     """{c[0]}       hMMMMMMMMMMMMMMMMMMh      """,
+     """{c[0]}   ..  yyyyyyyyyyyyyyyyyyyy  ..  """,
+     """{c[0]} .mMMm`MMMMMMMMMMMMMMMMMMMM`mMMm.""",
+     """{c[0]} :MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:""",
+     """{c[0]} :MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:""",
+     """{c[0]} :MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:""",
+     """{c[0]} :MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:""",
+     """{c[0]} -MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM-""",
+     """{c[0]}  +yy+ MMMMMMMMMMMMMMMMMMMM +yy+ """,
+     """{c[0]}       mMMMMMMMMMMMMMMMMMMm      """,
+     """{c[0]}       `/++MMMMh++hMMMM++/`      """,
+     """{c[0]}           MMMMo  oMMMM          """,
+     """{c[0]}           MMMMo  oMMMM          """,
+     """{c[0]}           oNMm-  -mMNs          """
 ]

archey/logos/arch.py
@@ -6,22 +6,22 @@ from archey.colors import Colors
 COLORS = [Colors.CYAN_BRIGHT, Colors.CYAN_NORMAL]
 
 LOGO = [
-    """{c[0]}               +                 """,
-    """{c[0]}               #                 """,
-    """{c[0]}              ###                """,
-    """{c[0]}             #####               """,
-    """{c[0]}             ######              """,
-    """{c[0]}            ; #####;             """,
-    """{c[0]}           +##.#####             """,
-    """{c[0]}          +##########            """,
-    """{c[0]}         ######{c[1]}#####{c[0]}##;          """,
-    """{c[0]}        ###{c[1]}############{c[0]}+         """,
-    """{c[0]}       #{c[1]}######   #######{c[0]}         """,
-    """{c[0]}     {c[1]}.######;     ;###;`".{c[0]}       """,
-    """{c[0]}    {c[1]}.#######;     ;#####.{c[0]}        """,
-    """{c[0]}    {c[1]}#########.   .########`{c[0]}      """,
-    """{c[0]}   {c[1]}######'           '######{c[0]}     """,
-    """{c[0]}  {c[1]};####                 ####;{c[0]}    """,
-    """{c[0]}  {c[1]}##'                     '##{c[0]}    """,
-    """{c[0]} {c[1]}#'                         `#{c[0]}   """
+     """{c[0]}               +              """,
+     """{c[0]}               #              """,
+     """{c[0]}              ###             """,
+     """{c[0]}             #####            """,
+     """{c[0]}             ######           """,
+     """{c[0]}            ; #####;          """,
+     """{c[0]}           +##.#####          """,
+     """{c[0]}          +##########         """,
+     """{c[0]}         ######{c[1]}#####{c[0]}##;       """,
+     """{c[0]}        ###{c[1]}############{c[0]}+      """,
+     """{c[0]}       #{c[1]}######   #######{c[0]}      """,
+     """{c[0]}     {c[1]}.######;     ;###;`".{c[0]}    """,
+     """{c[0]}    {c[1]}.#######;     ;#####.{c[0]}     """,
+     """{c[0]}    {c[1]}#########.   .########`{c[0]}   """,
+     """{c[0]}   {c[1]}######'           '######{c[0]}  """,
+     """{c[0]}  {c[1]};####                 ####;{c[0]} """,
+     """{c[0]}  {c[1]}##'                     '##{c[0]} """,
+     """{c[0]} {c[1]}#'                         `#{c[0]}"""
 ]

archey/logos/bunsenlabs.py
@@ -6,26 +6,26 @@ from archey.colors import Colors
 COLORS = [Colors.WHITE_BRIGHT, Colors.YELLOW_BRIGHT, Colors.YELLOW_NORMAL]
 
 LOGO = [
-    """{c[0]}           .{c[1]}..{c[0]}+hhy+-`         """,
-    """{c[0]}        `+hd{c[1]}-{c[0]}+dddd{c[2]}hyso{c[0]}+//:    """,
-    """{c[0]}      `+dddd{c[1]}:-{c[0]}sdh/.           """,
-    """{c[0]}     -hdddddh{c[1]}-.{c[2]}/:{c[0]}             """,
-    """{c[0]}    /ddddddddd{c[1]}:```{c[0]}            """,
-    """{c[0]}   :ddddddddddd/              """,
-    """{c[0]}  `hdddddddddddd+             """,
-    """{c[0]}  /dddddddddddddd:            """,
-    """{c[0]}  odddds..sddddddh            """,
-    """{c[0]}  oddd/    /dddddd:           """,
-    """{c[0]}  +dd+      +ddddd+           """,
-    """{c[0]}  .dd`      `ddddd+           """,
-    """{c[0]}   oh        ydddd:           """,
-    """{c[0]}   `o        sdddh`           """,
-    """{c[0]}             yddd:            """,
-    """{c[0]}            `dddo             """,
-    """{c[0]}     :s     :dds              """,
-    """{c[0]}     yd/    yd+               """,
-    """{c[0]}   `sddy   :h-                """,
-    """{c[0]}  `sddys`  :`                 """,
-    """{c[0]} -hdy+`y+yo./+/:-             """,
-    """{c[0]} ...  .o++oso+/               """
+     """{c[0]}           .{c[1]}..{c[0]}+hhy+-`      """,
+     """{c[0]}        `+hd{c[1]}-{c[0]}+dddd{c[2]}hyso{c[0]}+//: """,
+     """{c[0]}      `+dddd{c[1]}:-{c[0]}sdh/.        """,
+     """{c[0]}     -hdddddh{c[1]}-.{c[2]}/:{c[0]}          """,
+     """{c[0]}    /ddddddddd{c[1]}:```{c[0]}         """,
+     """{c[0]}   :ddddddddddd/           """,
+     """{c[0]}  `hdddddddddddd+          """,
+     """{c[0]}  /dddddddddddddd:         """,
+     """{c[0]}  odddds..sddddddh         """,
+     """{c[0]}  oddd/    /dddddd:        """,
+     """{c[0]}  +dd+      +ddddd+        """,
+     """{c[0]}  .dd`      `ddddd+        """,
+     """{c[0]}   oh        ydddd:        """,
+     """{c[0]}   `o        sdddh`        """,
+     """{c[0]}             yddd:         """,
+     """{c[0]}            `dddo          """,
+     """{c[0]}     :s     :dds           """,
+     """{c[0]}     yd/    yd+            """,
+     """{c[0]}   `sddy   :h-             """,
+     """{c[0]}  `sddys`  :`              """,
+     """{c[0]} -hdy+`y+yo./+/:-          """,
+     """{c[0]} ...  .o++oso+/            """
 ]

archey/logos/centos.py
@@ -13,23 +13,23 @@ COLORS = [
 
 # pylint: disable=line-too-long
 LOGO = [
-    """{c[0]}                  {c[1]}..{c[0]}                    """,
-    """{c[0]}                {c[1]}.PLTJ.{c[0]}                  """,
-    """{c[0]}               {c[1]}<><><><>{c[0]}                 """,
-    """{c[0]}      {c[2]}KKSSV' 4KKK{c[0]} {c[1]}LJ{c[0]} {c[4]}KKKL.'VSSKK{c[0]}        """,
-    """{c[0]}      {c[2]}KKV' 4KKKKK{c[0]} {c[1]}LJ{c[0]} {c[4]}KKKKAL 'VKK{c[0]}        """,
-    """{c[0]}      {c[2]}V' ' 'VKKKK{c[0]} {c[1]}LJ{c[0]} {c[4]}KKKKV' ' 'V{c[0]}        """,
-    """{c[0]}      {c[2]}.4MA.' 'VKK{c[0]} {c[1]}LJ{c[0]} {c[4]}KKV' '.4Mb.{c[0]}        """,
-    """{c[0]}    {c[4]}.{c[0]} {c[2]}KKKKKA.' 'V{c[0]} {c[1]}LJ{c[0]} {c[4]}V' '.4KKKKK{c[0]} {c[3]}.{c[0]}      """,
-    """{c[0]}  {c[4]}.4D{c[0]} {c[2]}KKKKKKKA.''{c[0]} {c[1]}LJ{c[0]} {c[4]}''.4KKKKKKK{c[0]} {c[3]}FA.{c[0]}    """,
-    """{c[0]} {c[4]}<QDD +++++++++++{c[0]}    {c[3]}+++++++++++ GFD>{c[0]}   """,
-    """{c[0]}  {c[4]}'VD{c[0]} {c[3]}KKKKKKKK'..{c[0]} {c[2]}LJ{c[0]} {c[1]}..'KKKKKKKK{c[0]} {c[3]}FV{c[0]}     """,
-    """{c[0]}    {c[4]}'{c[0]} {c[3]}VKKKKK'. .4{c[0]} {c[2]}LJ{c[0]} {c[1]}K. .'KKKKKV{c[0]} {c[3]}'{c[0]}      """,
-    """{c[0]}      {c[3]} 'VK'. .4KK{c[0]} {c[2]}LJ{c[0]} {c[1]}KKA. .'KV' {c[0]}        """,
-    """{c[0]}      {c[3]}A. . .4KKKK{c[0]} {c[2]}LJ{c[0]} {c[1]}KKKKA. . .4{c[0]}        """,
-    """{c[0]}      {c[3]}KKA. 'KKKKK{c[0]} {c[2]}LJ{c[0]} {c[1]}KKKKK' .4KK{c[0]}        """,
-    """{c[0]}      {c[3]}KKSSA. VKKK{c[0]} {c[2]}LJ{c[0]} {c[1]}KKKV .4SSKK{c[0]}        """,
-    """{c[0]}               {c[2]}<><><><>{c[0]}                 """,
-    """{c[0]}                {c[2]}'MKKM'{c[0]}                  """,
-    """{c[0]}                  {c[2]}''{c[0]}                    """
+     """{c[0]}                  {c[1]}..{c[0]}                 """,
+     """{c[0]}                {c[1]}.PLTJ.{c[0]}               """,
+     """{c[0]}               {c[1]}<><><><>{c[0]}              """,
+     """{c[0]}      {c[2]}KKSSV' 4KKK{c[0]} {c[1]}LJ{c[0]} {c[4]}KKKL.'VSSKK{c[0]}     """,
+     """{c[0]}      {c[2]}KKV' 4KKKKK{c[0]} {c[1]}LJ{c[0]} {c[4]}KKKKAL 'VKK{c[0]}     """,
+     """{c[0]}      {c[2]}V' ' 'VKKKK{c[0]} {c[1]}LJ{c[0]} {c[4]}KKKKV' ' 'V{c[0]}     """,
+     """{c[0]}      {c[2]}.4MA.' 'VKK{c[0]} {c[1]}LJ{c[0]} {c[4]}KKV' '.4Mb.{c[0]}     """,
+     """{c[0]}    {c[4]}.{c[0]} {c[2]}KKKKKA.' 'V{c[0]} {c[1]}LJ{c[0]} {c[4]}V' '.4KKKKK{c[0]} {c[3]}.{c[0]}   """,
+     """{c[0]}  {c[4]}.4D{c[0]} {c[2]}KKKKKKKA.''{c[0]} {c[1]}LJ{c[0]} {c[4]}''.4KKKKKKK{c[0]} {c[3]}FA.{c[0]} """,
+     """{c[0]} {c[4]}<QDD +++++++++++{c[0]}    {c[3]}+++++++++++ GFD>{c[0]}""",
+     """{c[0]}  {c[4]}'VD{c[0]} {c[3]}KKKKKKKK'..{c[0]} {c[2]}LJ{c[0]} {c[1]}..'KKKKKKKK{c[0]} {c[3]}FV{c[0]}  """,
+     """{c[0]}    {c[4]}'{c[0]} {c[3]}VKKKKK'. .4{c[0]} {c[2]}LJ{c[0]} {c[1]}K. .'KKKKKV{c[0]} {c[3]}'{c[0]}   """,
+     """{c[0]}      {c[3]} 'VK'. .4KK{c[0]} {c[2]}LJ{c[0]} {c[1]}KKA. .'KV' {c[0]}     """,
+     """{c[0]}      {c[3]}A. . .4KKKK{c[0]} {c[2]}LJ{c[0]} {c[1]}KKKKA. . .4{c[0]}     """,
+     """{c[0]}      {c[3]}KKA. 'KKKKK{c[0]} {c[2]}LJ{c[0]} {c[1]}KKKKK' .4KK{c[0]}     """,
+     """{c[0]}      {c[3]}KKSSA. VKKK{c[0]} {c[2]}LJ{c[0]} {c[1]}KKKV .4SSKK{c[0]}     """,
+     """{c[0]}               {c[2]}<><><><>{c[0]}              """,
+     """{c[0]}                {c[2]}'MKKM'{c[0]}               """,
+     """{c[0]}                  {c[2]}''{c[0]}                 """
 ]

archey/logos/crunchbang.py
@@ -6,20 +6,20 @@ from archey.colors import Colors
 COLORS = [Colors.WHITE_BRIGHT]
 
 LOGO = [
-    """{c[0]}                 ___       ___      _    """,
-    """{c[0]}                /  /      /  /     | |   """,
-    """{c[0]}               /  /      /  /      | |   """,
-    """{c[0]}              /  /      /  /       | |   """,
-    """{c[0]}      _______/  /______/  /______  | |   """,
-    """{c[0]}     /______   _______   _______/  | |   """,
-    """{c[0]}           /  /      /  /          | |   """,
-    """{c[0]}          /  /      /  /           | |   """,
-    """{c[0]}         /  /      /  /            | |   """,
-    """{c[0]}  ______/  /______/  /______       | |   """,
-    """{c[0]} /_____   _______   _______/       | |   """,
-    """{c[0]}      /  /      /  /               | |   """,
-    """{c[0]}     /  /      /  /                |_|   """,
-    """{c[0]}    /  /      /  /                  _    """,
-    """{c[0]}   /  /      /  /                  | |   """,
-    """{c[0]}  /__/      /__/                   |_|   """
+     """{c[0]}                 ___       ___      _ """,
+     """{c[0]}                /  /      /  /     | |""",
+     """{c[0]}               /  /      /  /      | |""",
+     """{c[0]}              /  /      /  /       | |""",
+     """{c[0]}      _______/  /______/  /______  | |""",
+     """{c[0]}     /______   _______   _______/  | |""",
+     """{c[0]}           /  /      /  /          | |""",
+     """{c[0]}          /  /      /  /           | |""",
+     """{c[0]}         /  /      /  /            | |""",
+     """{c[0]}  ______/  /______/  /______       | |""",
+     """{c[0]} /_____   _______   _______/       | |""",
+     """{c[0]}      /  /      /  /               | |""",
+     """{c[0]}     /  /      /  /                |_|""",
+     """{c[0]}    /  /      /  /                  _ """,
+     """{c[0]}   /  /      /  /                  | |""",
+     """{c[0]}  /__/      /__/                   |_|"""
 ]

archey/logos/darwin.py
@@ -6,23 +6,23 @@ from archey.colors import Colors
 COLORS = [Colors.WHITE_NORMAL]
 
 LOGO = [
-    """{c[0]}                     'c.          """,
-    """{c[0]}                  ,xNMM.          """,
-    """{c[0]}                .OMMMMo           """,
-    """{c[0]}                OMMM0,            """,
-    """{c[0]}      .;loddo:' loolloddol;.      """,
-    """{c[0]}    cKMMMMMMMMMMNWMMMMMMMMMM0:    """,
-    """{c[0]}  .KMMMMMMMMMMMMMMMMMMMMMMMWd.    """,
-    """{c[0]}  XMMMMMMMMMMMMMMMMMMMMMMMX.      """,
-    """{c[0]}  MMMMMMMMMMMMMMMMMMMMMMMM:       """,
-    """{c[0]}  MMMMMMMMMMMMMMMMMMMMMMMM:       """,
-    """{c[0]}  MMMMMMMMMMMMMMMMMMMMMMMMX.      """,
-    """{c[0]}  kMMMMMMMMMMMMMMMMMMMMMMMMWd.    """,
-    """{c[0]}  .XMMMMMMMMMMMMMMMMMMMMMMMMMMk   """,
-    """{c[0]}   .XMMMMMMMMMMMMMMMMMMMMMMMMK.   """,
-    """{c[0]}     kMMMMMMMMMMMMMMMMMMMMMMd     """,
-    """{c[0]}      ;KMMMMMMMWXXWMMMMMMMk.      """,
-    """{c[0]}        .cooc,.    .,coo:.        """
+     """{c[0]}                     'c.       """,
+     """{c[0]}                  ,xNMM.       """,
+     """{c[0]}                .OMMMMo        """,
+     """{c[0]}                OMMM0,         """,
+     """{c[0]}      .;loddo:' loolloddol;.   """,
+     """{c[0]}    cKMMMMMMMMMMNWMMMMMMMMMM0: """,
+     """{c[0]}  .KMMMMMMMMMMMMMMMMMMMMMMMWd. """,
+     """{c[0]}  XMMMMMMMMMMMMMMMMMMMMMMMX.   """,
+     """{c[0]}  MMMMMMMMMMMMMMMMMMMMMMMM:    """,
+     """{c[0]}  MMMMMMMMMMMMMMMMMMMMMMMM:    """,
+     """{c[0]}  MMMMMMMMMMMMMMMMMMMMMMMMX.   """,
+     """{c[0]}  kMMMMMMMMMMMMMMMMMMMMMMMMWd. """,
+     """{c[0]}  .XMMMMMMMMMMMMMMMMMMMMMMMMMMk""",
+     """{c[0]}   .XMMMMMMMMMMMMMMMMMMMMMMMMK.""",
+     """{c[0]}     kMMMMMMMMMMMMMMMMMMMMMMd  """,
+     """{c[0]}      ;KMMMMMMMWXXWMMMMMMMk.   """,
+     """{c[0]}        .cooc,.    .,coo:.     """
 ]
 
 # Alternative logo : Apple's "retro" logo.
@@ -38,17 +38,17 @@ COLORS_RETRO = [
 ]
 
 LOGO_RETRO = [
-    """{c[0]}                {c[1]}###{c[0]}               """,
-    """{c[0]}              {c[1]}####{c[0]}                """,
-    """{c[0]}              {c[1]}###{c[0]}                 """,
-    """{c[0]}      {c[1]}#######{c[0]}    {c[1]}#######{c[0]}          """,
-    """{c[0]}    {c[2]}######################{c[0]}        """,
-    """{c[0]}   {c[2]}#####################{c[0]}          """,
-    """{c[0]}   {c[4]}####################{c[0]}           """,
-    """{c[0]}   {c[4]}####################{c[0]}           """,
-    """{c[0]}   {c[4]}#####################{c[0]}          """,
-    """{c[0]}    {c[5]}######################{c[0]}        """,
-    """{c[0]}     {c[5]}####################{c[0]}         """,
-    """{c[0]}       {c[6]}################{c[0]}           """,
-    """{c[0]}        {c[6]}####{c[0]}     {c[6]}#####{c[0]}            """,
+     """{c[0]}               {c[1]}###{c[0]}       """,
+     """{c[0]}             {c[1]}####{c[0]}        """,
+     """{c[0]}             {c[1]}###{c[0]}         """,
+     """{c[0]}     {c[1]}#######{c[0]}    {c[1]}#######{c[0]}  """,
+     """{c[0]}   {c[2]}######################{c[0]}""",
+     """{c[0]}  {c[2]}#####################{c[0]}  """,
+     """{c[0]}  {c[4]}####################{c[0]}   """,
+     """{c[0]}  {c[4]}####################{c[0]}   """,
+     """{c[0]}  {c[4]}#####################{c[0]}  """,
+     """{c[0]}   {c[5]}######################{c[0]}""",
+     """{c[0]}    {c[5]}####################{c[0]} """,
+     """{c[0]}      {c[6]}################{c[0]}   """,
+     """{c[0]}       {c[6]}####{c[0]}     {c[6]}#####{c[0]}    """
 ]

commit db22230bb6b5a753d5de8229d7352f5a6c232207

[OUTPUT] Fixes text width overlap regression introduced in 9d337e8
[skip ci]

archey/output.py
@@ -24,6 +24,9 @@ class Output:
     This is the object handling output entries populating.
     It also handles the logo choice based on some system detections.
     """
+
+    __LOGO_RIGHT_PADDING = '   '
+
     def __init__(self, **kwargs):
         # Fetches passed arguments.
         self._format_to_json = kwargs.get('format_to_json')
@@ -112,7 +115,7 @@ class Output:
         if not sys.stdout.isatty():
             text_width = float('inf')
         else:
-            text_width = get_terminal_size().columns - logo_width
+            text_width = get_terminal_size().columns - logo_width - len(self.__LOGO_RIGHT_PADDING)
 
         text_wrapper = TextWrapper(
             width=text_width,
@@ -154,7 +157,7 @@ class Output:
 
         # Merge entry results to the distribution logo.
         logo_with_entries = os.linesep.join([
-            f"{logo_part}   {entry_part}"
+            f"{logo_part}{self.__LOGO_RIGHT_PADDING}{entry_part}"
             for logo_part, entry_part in zip(self._logo, self._results)
         ])
 