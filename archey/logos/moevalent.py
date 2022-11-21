8ec3d6c9610dea73c591e678911e774ca272e78c
Jin Asanami <101626649+Jin-Asanami@users.noreply.github.com>
2022-11-21 22:19:12 +0900

    Add Moevalent

---
+++ archey/logos/moevalent.py
@@ -0,0 +1,23 @@
+"""Moevalent logo"""
+
+from archey.colors import Colors
+
+COLORS = [Colors.RED_BRIGHT, Colors.MAGENTA_BRIGHT, Colors.WHITE_BRIGHT]
+
+# pylint: disable=line-too-long
+LOGO = [
+    """{c[0]}UUUUU{c[1]}VVVVVVVVVVVVVVVVVVVVVVV{c[0]}UUUUU""",
+    """{c[0]}UUUUUU{c[1]}VVVVVVVV{c[2]}MM{c[1]}V{c[2]}MMVVVVVVVV{c[0]}UUUUUU""",
+    """{c[0]}UUUUUUU{c[1]}VVVVVV{c[2]}MMMMMMM{c[1]}VVVVVV{c[0]}UUUUUUU""",
+    """{c[0]}UUUUUUU {c[1]}VVVVVV{c[2]}MMMMM{c[1]}VVVVVV {c[0]}UUUUUUU""",
+    """{c[0]}UUUUUUUEE{c[1]}VVVVVV{c[2]}MMM{c[1]}VVVVVV  {c[0]}UUUUUUU""",
+    """{c[0]}UUUUUUUEEE{c[1]}VVVVVV{c[2]}M{c[1]}VVVVVV   {c[0]}UUUUUUU""",
+    """{c[0]}UUUUUUU    {c[1]}VVVVVVVVVVV    {c[0]}UUUUUUU""",
+    """{c[0]}UUUUUUUEEEEEEEEEEEEEE     UUUUUUU""",
+    """{c[0]}UUUUUUUEEEEEEEEEEEEEE     UUUUUUU""",
+    """{c[0]}UUUUUUU       {c[1]}VVVVV       {c[0]}UUUUUUU""",
+    """{c[0]}UUUUUUU        {c[1]}VVV        {c[0]}UUUUUUU""",
+    """{c[0]}   UUUUUUU      {c[1]}V      {c[0]}UUUUUUU""",
+    """{c[0]}      UUUUUUUUUUUUUUUUUUU""",
+    """{c[0]}         UUUUUUUUUUUUU""",
+]
