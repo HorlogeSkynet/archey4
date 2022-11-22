676bdc218f119e313bf79511480dcdec66cd0a3d
Jin Asanami <101626649+Jin-Asanami@users.noreply.github.com>
2022-11-23 03:17:33 +0900

    [LOGO] Adds support for Univalent and Moevalent (#125)

---
+++ archey/logos/univalent.py
@@ -0,0 +1,22 @@
+"""Univalent logo"""
+
+from archey.colors import Colors
+
+COLORS = [Colors.CYAN_BRIGHT, Colors.GREEN_BRIGHT]
+
+LOGO = [
+    """{c[0]}UUUUU{c[1]}VVVVVVVVVVVVVVVVVVVVVVV{c[0]}UUUUU""",
+    """{c[0]}UUUUUU{c[1]}VVVVVVVVVVVVVVVVVVVVV{c[0]}UUUUUU""",
+    """{c[0]}UUUUUUU{c[1]}VVVVVVVVVVVVVVVVVVV{c[0]}UUUUUUU""",
+    """{c[0]}UUUUUUU {c[1]}VVVVVVVVVVVVVVVVV {c[0]}UUUUUUU""",
+    """{c[0]}UUUUUUUEE{c[1]}VVVVVVVVVVVVVVV  {c[0]}UUUUUUU""",
+    """{c[0]}UUUUUUUEEE{c[1]}VVVVVVVVVVVVV   {c[0]}UUUUUUU""",
+    """{c[0]}UUUUUUU    {c[1]}VVVVVVVVVVV    {c[0]}UUUUUUU""",
+    """{c[0]}UUUUUUUEEEEEEEEEEEEEE     UUUUUUU""",
+    """{c[0]}UUUUUUUEEEEEEEEEEEEEE     UUUUUUU""",
+    """{c[0]}UUUUUUU       {c[1]}VVVVV       {c[0]}UUUUUUU""",
+    """{c[0]}UUUUUUU        {c[1]}VVV        {c[0]}UUUUUUU""",
+    """{c[0]}   UUUUUUU      {c[1]}V      {c[0]}UUUUUUU   """,
+    """{c[0]}      UUUUUUUUUUUUUUUUUUU        """,
+    """{c[0]}         UUUUUUUUUUUUU           """,
+]
