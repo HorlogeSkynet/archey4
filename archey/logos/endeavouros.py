a16a91b685bc4064565657837ea855482b263c18
Tobilike <80016610+Tobilike@users.noreply.github.com>
2024-01-05 00:11:15 +0100

    Add EndeavourOS logo

---
+++ archey/logos/endeavouros.py
@@ -0,0 +1,27 @@
+# Endeavour OS Logo
+
+from archey.colors import Colors
+
+COLORS = [Colors.RED_BRIGHT, Colors.MAGENTA_NORMAL, Colors.BLUE_NORMAL]
+
+LOGO = [
+    """                                        """,
+    """                                        """,
+    f"""{COLORS[0]}                     ./{COLORS[1]}o{COLORS[2]}.               """,
+    f"""{COLORS[0]}                   ./{COLORS[1]}sssso{COLORS[2]}-             """,
+    f"""{COLORS[0]}                 `:{COLORS[1]}osssssss+{COLORS[2]}-           """,
+    f"""{COLORS[0]}               `:+{COLORS[1]}sssssssssso{COLORS[2]}/.         """,
+    f"""{COLORS[0]}             `-/{COLORS[1]}ssssssssssssso{COLORS[2]}/.        """,
+    f"""{COLORS[0]}           `-/+{COLORS[1]}sssssssssssssssso{COLORS[2]}+:`     """,
+    f"""{COLORS[0]}         `-:/+{COLORS[1]}sssssssssssssssssso{COLORS[2]}/.     """,
+    f"""{COLORS[0]}       `.://o{COLORS[1]}sssssssssssssssssssso{COLORS[2]}++-   """,
+    f"""{COLORS[0]}      .://+{COLORS[1]}ssssssssssssssssssssssso{COLORS[2]}++:  """,
+    f"""{COLORS[0]}    .:///o{COLORS[1]}ssssssssssssssssssssssssso{COLORS[2]}++: """,
+    f"""{COLORS[0]}  `:////{COLORS[1]}ssssssssssssssssssssssssssso{COLORS[2]}+++.""",
+    f"""{COLORS[0]}`-////+{COLORS[1]}ssssssssssssssssssssssssssso{COLORS[2]}++++-""",
+    f"""{COLORS[0]} `..-+{COLORS[1]}oosssssssssssssssssssssssso{COLORS[2]}+++++/`""",
+    f"""{COLORS[2]}   ./++++++++++++++++++++++++++++++/:.  """,
+    f"""{COLORS[2]}  `:::::::::::::::::::::::::------``    """
+    ]
+
+
